9d1d72a371eaf1a232e6d5ef3caa73c769f06b52
tomasbalvocius <tomasbalv@gmail.com>
2023-07-19 15:00:25 +0300

    angular-material

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  OnDestroy,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../auth/auth-service';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css'],
+})
+export class SidenavListComponent implements OnInit, OnDestroy {
+  isAuth = false;
+  authSubscription: Subscription;
+  @Output() closeSidenav = new EventEmitter<void>();
+  constructor(private authService: AuthService) {}
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+  onClose() {
+    this.closeSidenav.emit();
+  }
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
+  }
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+}
---
+++ src/app/training/new-training/new-training.component.ts
@@ -0,0 +1,22 @@
+import { Exercise } from './../exercise.model';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { TrainingService } from '../training.service';
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.css'],
+})
+export class NewTrainingComponent implements OnInit {
+  @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
+
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
+  }
+  onStartTraining() {
+    this.trainingStart.emit();
+  }
+}
